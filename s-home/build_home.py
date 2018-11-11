5d66fe0b61c1d3e856a407a2d79de7c4ec59e7d6
John Oram <john.c.oram@gmail.com>
2018-11-11 10:59:34 -0800

    WIP: s-home serving index page and proxying

---
+++ s-home/build_home.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+import requests
+import yaml
+import os
+import jinja2
+
+
+def render(tpl_path, context):
+    path, filename = os.path.split(tpl_path)
+    return jinja2.Environment(
+        loader=jinja2.FileSystemLoader(path or './')
+    ).get_template(filename).render(context)
+
+
+class Service(object):
+
+    def __init__(self, name, icon_url, port):
+        self.name = name
+        self.icon_url = icon_url
+        self.port = port
+        download_file(icon_url, "./src/icons/{}.png".format(name).lower())
+    
+    @property
+    def url(self):
+        return "http://192.168.1.7:{port}".format(port=self.port)
+
+
+def get_services():
+    with open("../docker-compose.yml") as f:
+        content = f.read()
+        services = yaml.load(content)
+
+    for details in services.values():
+        name = ""
+        icon_url = ""
+        port = ""
+        env = details.get("environment", [])
+        for e in env:
+            if "=" in e:
+                k,v = e.split("=")
+                if k == "SERENITY_NAME":
+                    name = v
+                if k == "SERENITY_ICON_URL":
+                    icon_url = v
+                if k == "SERENITY_PORT":
+                    port = v
+        yield Service(name, icon_url, port)
+
+
+def download_file(url, path):
+    if os.path.exists(path):
+        return
+    print "downloading {} to {}".format(url, path)
+    r = requests.get(url, allow_redirects=True)
+    open(path, 'wb').write(r.content)
+
+
+def build_index(services):
+    context = {"services": services}
+    content = render("./templates/index.html", context)
+    with open("./src/index.html", "w") as f:
+        f.write(content)
+
+def build_nginx(services):
+    context = {"services": services}
+    content = render("./templates/nginx_config", context)
+    with open("./.nginx_serenity", "w") as f:
+        f.write(content)
+
+if __name__ == "__main__":
+    services = list(get_services())
+    build_index(services)
+    build_nginx(services)
+        
